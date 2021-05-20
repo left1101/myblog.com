4daf0fc86e7cca86d76256ef2dfd51fc20d9cee3
left1101 <xiru.xie@xiaoniangao.cn>
2021-05-20 22:34:40 +0800

    MOD 修改代码

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,16 +3,15 @@ import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
-import Snake from '../components/Snake'
+// import Snake from '../components/Snake'
 
 const IndexPage: React.FC = () => (
   <Layout>
-    <SEO title="Lihs Home" />
-    <h1>你好，World</h1>
+    <SEO title="Gatsby Default Starter" />
+    <h1>Hello，World</h1>
     <Link to="/blog/">
       <h1>Go to blog</h1>
     </Link>
-    <Snake />
 
   </Layout>
 )
